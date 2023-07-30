eafabe292ae262d444d0421777a3f3620815ba55
nocelly <140983116+nocelly@users.noreply.github.com>
2023-07-30 15:54:38 -0700

    player

--- player.gd
+++ player.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
-
+var Player_bullet = preload("res://player_bullet.tscn")
 
 var speed = 500
 var volasity = Vector2.ZERO
@@ -7,3 +7,7 @@ func _physics_process(delta):
 	volasity = Input.get_vector("left_move","right_move","up_move","down_move") * speed
 	move_and_slide(volasity)
 	look_at(get_global_mouse_position())
+	
+	
+	var bullet = Player_bullet.instance()
+	
