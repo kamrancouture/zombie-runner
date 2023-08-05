b3eb228714795e898b1d283385c2589decde481f
nocelly <140983116+nocelly@users.noreply.github.com>
2023-08-04 22:07:56 -0700

    tank_look_at
    tank_can_move

--- player.gd
+++ player.gd
@@ -5,8 +5,12 @@ var speed = 500
 var volasity = Vector2.ZERO
 func _physics_process(delta):
 	volasity = Input.get_vector("left_move","right_move","up_move","down_move") * speed
+	if not volasity.x == 0 or not volasity.y == 0:
+		$TankBodyDark.rotation = volasity.angle()
+		$TankBodyDark.rotation -= deg2rad(90)
 	move_and_slide(volasity)
-	look_at(get_global_mouse_position())
+	$TankDark.look_at(get_global_mouse_position())
+	$TankDark.rotation -= deg2rad(90)
 	
 	
 	var bullet = Player_bullet.instance()
