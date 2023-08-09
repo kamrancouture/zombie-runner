28232b07f930470dab4200e0ae88c892f56fb081
nocelly <140983116+nocelly@users.noreply.github.com>
2023-08-08 22:56:41 -0700

    health

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var fire_rate = .25
 var Player_bullet = preload("res://player_bullet.tscn")
+var health = 10
 var can_shoot = true
 var speed = 500
 var velocity = Vector2.ZERO
