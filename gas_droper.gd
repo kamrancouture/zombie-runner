84432ea3c3f774baa4d06d2b80e848d2b469c1c7
nocelly <140983116+nocelly@users.noreply.github.com>
2023-08-11 02:05:42 -0700

    kjkajkbnrg

--- gas_droper.gd
+++ gas_droper.gd
@@ -4,15 +4,16 @@ onready var player = get_parent().get_node("player")
 onready var bullet = get_parent().get_node("player_bullet")
 var tuching_player = false
 var damage = 80
-var health = 2
+var health = 1
 
 func _physics_process(delta):
-	if tuching_player == false:
-		look_at(player.global_position)
-		global_position += Vector2(speed , 0).rotated(rotation)
-	
+	if not Global.player_dead:
+		if tuching_player == false:
+			look_at(player.global_position)
+			global_position += Vector2(speed , 0).rotated(rotation)
+		
+			
 
 func _on_Area2D_body_entered(body):
-	player.health -= damage
-	bullet.bullet_damage =- health
-	queue_free()
+#	if player.health -= damage:
+		queue_free()
