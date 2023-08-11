197e2bc862bbb80884e4138d022ecc25078c5f03
nocelly <140983116+nocelly@users.noreply.github.com>
2023-08-11 00:53:58 -0700

    bullet go puff

--- player_bullet.gd
+++ player_bullet.gd
@@ -4,8 +4,10 @@ var speed = 1000
 var bullet_damage = 2
 
 
+
 func _physics_process(delta):
 	move_and_slide(Vector2(0 , speed).rotated(rotation))
+	
 	
 	
 
@@ -14,3 +16,9 @@ func _on_Area2D_body_entered(body):
 	body.health -= bullet_damage
 	queue_free()
 	
+
+
+
+
+func _on_Timer_timeout():
+	queue_free()
