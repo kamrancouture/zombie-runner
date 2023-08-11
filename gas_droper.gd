5ed4863ec626051b77bbf6fd7163a1bff56059a6
nocelly <140983116+nocelly@users.noreply.github.com>
2023-08-11 09:45:30 -0700

    gas dropper works

--- gas_droper.gd
+++ gas_droper.gd
@@ -12,8 +12,12 @@ func _physics_process(delta):
 			look_at(player.global_position)
 			global_position += Vector2(speed , 0).rotated(rotation)
 		
-			
+		
 
 func _on_Area2D_body_entered(body):
-#	if player.health -= damage:
-		queue_free()
+	player.health -= damage
+	queue_free()
+	
+
+func _on_Area2D2_body_entered(body):
+	queue_free()
