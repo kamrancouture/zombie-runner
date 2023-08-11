765c94eecc32d75db2458d102f3549aa2f9a33ce
nocelly <140983116+nocelly@users.noreply.github.com>
2023-08-11 10:26:38 -0700

    ;0

--- gas_droper.gd
+++ gas_droper.gd
@@ -2,17 +2,24 @@ extends KinematicBody2D
 var speed = 3
 onready var player = get_parent().get_node("player")
 onready var bullet = get_parent().get_node("player_bullet")
-var tuching_player = false
 var damage = 80
 var health = 1
 
+
+
 func _physics_process(delta):
+	
 	if not Global.player_dead:
-		if tuching_player == false:
-			look_at(player.global_position)
-			global_position += Vector2(speed , 0).rotated(rotation)
+		
+			
+		
+		look_at(player.global_position)
+		global_position += Vector2(speed , 0).rotated(rotation)
 		
 		
+
+
+
 
 func _on_Area2D_body_entered(body):
 	player.health -= damage
@@ -21,3 +28,4 @@ func _on_Area2D_body_entered(body):
 
 func _on_Area2D2_body_entered(body):
 	queue_free()
+	
