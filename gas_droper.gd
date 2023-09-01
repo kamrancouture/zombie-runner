d21d1c9a3235c56a60e2eb9f8e9d9519bb321b6c
nocelly <140983116+nocelly@users.noreply.github.com>
2023-09-01 07:25:22 -0700

    gas

--- gas_droper.gd
+++ gas_droper.gd
@@ -5,13 +5,17 @@ onready var bullet = get_parent().get_node("player_bullet")
 var damage = 80
 var health = 1
 
-
+var respound = true
 
 func _physics_process(delta):
 	
+#	if respound == true:
+#		get_parent().add_child()
+	
 	if not Global.player_dead:
 		
-			
+		
+		
 		
 		look_at(player.global_position)
 		global_position += Vector2(speed , 0).rotated(rotation)
@@ -19,13 +23,11 @@ func _physics_process(delta):
 		
 
 
-
-
 func _on_Area2D_body_entered(body):
 	player.health -= damage
 	queue_free()
-	
+	respound = true
 
 func _on_Area2D2_body_entered(body):
 	queue_free()
-	
+	respound = true
