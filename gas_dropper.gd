18d164a53590d010662dd1ecfa37bb7fc79e4bf4
nocelly <140983116+nocelly@users.noreply.github.com>
2023-09-04 20:19:11 -0700

    spawned

--- gas_dropper.gd
+++ gas_dropper.gd
@@ -4,14 +4,18 @@ onready var player = get_parent().get_node("player")
 onready var bullet = get_parent().get_node("player_bullet")
 var damage = 80
 var health = 1
-
+var spawned = 10
 func _physics_process(delta):
+	print(spawned)
 	
 	if health <= 0:
-		var gas_dropper = self.duplicate()
-		gas_dropper.global_position = Vector2.ZERO
-		get_parent().add_child(gas_dropper)
 		queue_free()
+		if spawned >= 0:
+			var gas_dropper = self.duplicate()
+			gas_dropper.global_position = Vector2.ZERO
+			get_parent().add_child(gas_dropper)
+			gas_dropper.spawned = spawned - 1
+		
 	
 	
 	if not Global.player_dead:
@@ -31,6 +35,7 @@ func _on_Area2D_body_entered(body):
 	gas_dropper.global_position = Vector2.ZERO
 	get_parent().add_child(gas_dropper)
 	queue_free()
+	spawned -= 1
 
 
 
